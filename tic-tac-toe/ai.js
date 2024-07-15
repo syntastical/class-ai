fe82e85dc4665dfdee6f1d57450bb0ac9addd18e
Sean Rester <syntastical@users.noreply.github.com>
2024-07-15 09:10:31 -0400

    refactoring

--- tic-tac-toe/ai.js
+++ tic-tac-toe/ai.js
@@ -12,32 +12,5 @@ state = [
 using console.log
 */
 function makeMove(state) {
-    
-    // // Opponent about to win top left
-    // if(state[1][0] == 'X' && state[2][0] == 'X' && state[0][0] == '') {
-    //     return [0, 0];
-    // }
 
-    // // Opponent about to win middle left
-    // if(state[0][0] == 'X' && state[2][0] == 'X' && state[1][0] == '') {
-    //     return [1, 0];
-    // }
-    
-    // // Opponent about to win middle bottom left
-    // if(state[0][0] == 'X' && state[1][0] == 'X' && state[2][0] == '') {
-    //     return [2, 0];
-    // }
-
-    // return [1, 1]
-
-    console.log(1)
-    if(state[1][0] === 'X' && state[2][0] === 'X' && state[0][0] === ''){
-        return[0,0]            
-    }            
-    console.log(2)
-    //opponent about to win (1,0)            
-    if(state[0][0] === 'X' && state[2][0] === 'X' && state[1][0] === ''){
-        return[1,0]            
-    }
-    console.log(3)
 }
