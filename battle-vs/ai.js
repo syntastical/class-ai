92b1d4e71a6b2e9245c64168a4ec99355f9f6dbe
Sean Rester <syntastical@users.noreply.github.com>
2024-07-18 11:27:47 -0400

    refactoring

---
+++ battle-vs/ai.js
@@ -0,0 +1,33 @@
+/*
+gameState = {
+    enemies: [
+        {
+            name: "name",
+            currentHitpoints: 123,
+            maxHitpoints: 234,
+            currentMagicpoints: 123,
+            maxMagicpoints: 234,
+            class(): string         Returns the class of the character (mage, warrior, cleric)
+        }
+    ]
+    allies: [
+        {
+            name: "",
+            currentHitpoints: 123,
+            maxHitpoints: 234,
+            currentMagicpoints: 25,
+            maxMagicpoints: 25
+            class(): string,        Returns the class of the character (mage, warrior, cleric)
+            attack(enemy),          Attacks a character
+            heal(ally),             Cleric only: heals a character for 13 hitpoints at the cost of 10 mana
+            revive(ally),           Cleric only: revives a fallen character to half health at the cost of 25 mana
+            fireball(enemy),        Mage only: cast fireball on a character, dealing 20 damage as the cost of 13 mana
+            sap(enemy),             Mage only: cast sap on a character, draining 7 hitpoints and 7 mana at the cost of 7 mana
+            spells()                Mage/Cleric only: returns a list of spells
+        }
+    ]
+}
+*/
+function alliesTurn(gameState) {
+    gameState.allies[0].attack(gameState.enemies[0]);
+}
